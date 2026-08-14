226c251b97fbde01c7c485e1916e1f09bb8c0a7c
Malaviya meet <123464502+macyman1@users.noreply.github.com>
2026-08-14 05:39:54 +0530

    Time: 20 ms (38.29%), Space: 108.3 MB (31.19%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) 
+    {
+    //    for(int i = 0 ; i<nums.length-1;i++)
+    //    {
+    //         for(int j=i+1;j<nums.length;j++)
+    //         {
+    //             if(nums[i]==nums[j])
+    //             {
+    //                 return true;
+    //             }
+    //         }
+
+    HashSet<Integer> set = new HashSet<>();
+     for(int i=0; i<nums.length; i++)
+    { 
+        if(set.contains(nums[i])) 
+        { 
+            return true;
+        }
+        else
+        { 
+            set.add(nums[i]);
+        } 
+    } 
+    return false; 
+    }
+         
+     
+}
